ca0bf9adb1c33b63f47927c8c355a7e896d0ce28
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-06-14 15:57:54 +0200

    Add ContactsView content

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,17 +1,53 @@
 <script>
 export default {
-  name: 'ContactsView'
+  name: "ContactsView",
 };
 </script>
 
-
 <template>
-  <div>
-    <h1>CONTACTS PAGE</h1>
-  </div>
+  <section class="contact-me">
+    <div class="container my-5">
+      <form>
+        <div class="mb-3">
+          <label for="name" class="form-label">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            class="form-control"
+            placeholder="Luca Ranieri"
+            aria-describedby="helpId"
+          />
+          <small id="nameHelper" class="text-muted"
+            >Type your full name here</small
+          >
+        </div>
+
+        <div class="mb-3">
+          <label for="email" class="form-label">Email</label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            class="form-control"
+            placeholder="Luca@mail.com"
+            aria-describedby="helpId"
+          />
+          <small id="emailHelper" class="text-muted"
+            >Type your email here</small
+          >
+        </div>
+
+        <div class="mb-3">
+          <div for="message" class="form-label">Message:</div>
+          <textarea name="message" id="message" cols="100" rows="8"></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary me-2">Send</button>
+        <button type="reset" class="btn btn-warning">Reset</button>
+      </form>
+    </div>
+  </section>
 </template>
 
-
-<style lang="sass" scoped>
-
-</style>
+<style lang="sass" scoped></style>
